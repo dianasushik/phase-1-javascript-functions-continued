a3dabdfcf4bc9942f2cf8aa2cb319b0bc69330e2
Diana Sushik <dianasushik2000@gmail.com>
2022-08-08 20:06:25 -0700

    commit tree 4b07af7e93cf1a88eaed43251bbc0614b6fcbe9c

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// code your solution here
+function saturdayFun(activity = 'roller-skate'){
+return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun();
+saturdayFun('bathe my dog');
+
+function mondayWork(activity = 'go to the office'){
+    return `This Monday, I will ${activity}.`;
+
+}
+mondayWork();
+mondayWork('work from home');
+
+
+function wrapAdjective(flair = '*'){
+        return function(adj = 'special') {
+            return `You are ${flair}${adj}${flair}!`
+        }
+    }
+
+    const encouragingPromptFunction =
+     wrapAdjective("!!!")
